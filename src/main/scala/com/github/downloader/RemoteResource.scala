f50f8e60bc83377b7fbb4887ab950e5669ae84f4
Pankaj Sharma <sharmapankaj2512@gmail.com>
2017-11-27 22:18:59 +0530

    Change signature to make offset as stream param

--- src/main/scala/com/github/downloader/RemoteResource.scala
+++ src/main/scala/com/github/downloader/RemoteResource.scala
@@ -7,17 +7,17 @@ import akka.stream.scaladsl.StreamConverters._
 
 import scala.util.Try
 
-case class RemoteResource(Url: String, offset: Int = 0) {
-  val connection: HttpURLConnection = httpRangeUrlConnection()
+case class RemoteResource(Url: String) {
+  def asStream(offset: Int = 0): Source[PartialResponse, Any] = {
+    val connection = httpRangeUrlConnection(offset)
 
-  def asStream(): Source[PartialResponse, Any] = {
     Try(connection.getInputStream)
       .map(stream => fromInputStream(() => stream)
         .map(PartialResponse(_, connection.getContentLength)))
       .getOrElse(Source.empty)
   }
 
-  private def httpRangeUrlConnection(): HttpURLConnection = {
+  private def httpRangeUrlConnection(offset: Int): HttpURLConnection = {
     val connection = new URL(Url).openConnection().asInstanceOf[HttpURLConnection]
     connection.setRequestProperty("Range", s"bytes=$offset-")
     connection
