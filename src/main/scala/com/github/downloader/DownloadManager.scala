339c3cb14f8b47fe9ed0c07105844a518664dec0
Pankaj Sharma <sharmapankaj2512@gmail.com>
2017-12-01 09:43:21 +0530

    Use broadcast to implement fan-out

--- src/main/scala/com/github/downloader/DownloadManager.scala
+++ src/main/scala/com/github/downloader/DownloadManager.scala
@@ -1,12 +1,27 @@
 package com.github.downloader
 
+import akka.Done
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Sink
+import akka.stream.{ActorMaterializer, ClosedShape}
+import akka.stream.scaladsl.{Broadcast, GraphDSL, RunnableGraph, Sink}
+
+import scala.concurrent.Future
 
 case class DownloadManager(progressBar: CommandLineProgressBar) {
   def startDownload(url: String)(implicit system: ActorSystem, materializer: ActorMaterializer) = {
     val stream = RemoteResource(url).asStream()
-    stream.runWith(Sink.foreach(pr => progressBar.tick(pr)))
+    val progressBarSink: Sink[PartialResponse, Future[Done]] = Sink.foreach(pr => progressBar.tick(pr))
+
+    RunnableGraph.fromGraph(GraphDSL.create(progressBarSink) { implicit builder =>
+      ps =>
+        import GraphDSL.Implicits._
+
+        val broadcast = builder.add(Broadcast[PartialResponse](1))
+
+        stream ~> broadcast.in
+        broadcast.out(0) ~> ps.in
+
+        ClosedShape
+    }).run()
   }
 }
--- src/main/scala/com/github/downloader/TestDrive.scala
+++ src/main/scala/com/github/downloader/TestDrive.scala
@@ -2,6 +2,7 @@ package com.github.downloader
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object TestDrive extends App {
   implicit val system = ActorSystem("reactive-tweets")
