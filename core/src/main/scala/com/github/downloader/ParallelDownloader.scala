272a6b17c4a4c280f2cbf93bf830a035e03e5af8
Pankaj Sharma <sharmapankaj2512@gmail.com>
2017-12-23 17:13:36 +0530

    Add progress bar subscriber for parallel download

--- core/src/main/scala/com/github/downloader/ParallelDownloader.scala
+++ core/src/main/scala/com/github/downloader/ParallelDownloader.scala
@@ -3,28 +3,38 @@ package com.github.downloader
 import java.io._
 import java.nio.file.Paths
 
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.FileIO
+import akka.stream.scaladsl.{Broadcast, FileIO, Sink}
+import akka.util.ByteString
+import com.github.subscriber.DownloadSubscriber
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-case class ParallelDownloader() {
+case class ParallelDownloader(subscriber: DownloadSubscriber) {
   implicit val system: ActorSystem = ActorSystem("downloader")
   implicit val materialize: ActorMaterializer = ActorMaterializer()
 
   def startDownload(url: String, offset: Long): Unit = {
     val fileName = Paths.get(url).getFileName.toString
     val partFileNamePrefix = s"part-$fileName"
+    val actualSize = RemoteResource(url).size()
 
     Future.sequence(RemoteResource(url).asParallelStream()
       .map(s => s.map(pr => pr.byteString))
-      .zipWithIndex.map({ case (stream, index) => FileIO.toPath(new File(s"$partFileNamePrefix-$index").toPath)
-      .runWith(stream)
-    })).onComplete(_ => {
-      FileMerger(partFileNamePrefix, fileName).merge()
-      system.terminate()
-    })
+      .zipWithIndex.map({ case (stream, index) => sink(partFileNamePrefix, index, actualSize).runWith(stream) }))
+      .onComplete(_ => {
+        FileMerger(partFileNamePrefix, fileName).merge()
+        subscriber.completed()
+        system.terminate()
+      })
+  }
+
+  private def sink(partFileNamePrefix: String, index: Int, actualSize: Int): Sink[ByteString, NotUsed] = {
+    val fileSink = FileIO.toPath(new File(s"$partFileNamePrefix-$index").toPath)
+    val subscriberSink = Sink.foreach[ByteString](bs => subscriber.notify(PartialResponse(bs, actualSize)))
+    Sink.combine(subscriberSink, fileSink)(Broadcast(_))
   }
 }
--- examples/src/main/scala/com/github/downloader/examples/ParallelDownloadVideoFile.scala
+++ examples/src/main/scala/com/github/downloader/examples/ParallelDownloadVideoFile.scala
@@ -1,9 +1,11 @@
 package com.github.downloader.examples
 
-import com.github.downloader.ParallelDownloader
+import com.github.downloader.{ParallelDownloader, RemoteResource}
+import com.github.subscriber.CommandLineProgressBar
 
 object ParallelDownloadVideoFile extends App {
   val url = "http://mirrors.standaloneinstaller.com/video-sample/jellyfish-25-mbps-hd-hevc.3gp"
+  val progressBar = CommandLineProgressBar(RemoteResource(url).size())
 
-  ParallelDownloader().startDownload(url, 0)
+  ParallelDownloader(progressBar).startDownload(url, 0)
 }
