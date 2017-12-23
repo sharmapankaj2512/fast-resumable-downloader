a7d6ea361e04d5e761bcff31853f9b5dc2f533be
Pankaj Sharma <sharmapankaj2512@gmail.com>
2017-12-23 13:01:41 +0530

    Extract and move file merging logic to its own class

--- core/src/main/scala/com/github/downloader/ParallelDownloader.scala
+++ core/src/main/scala/com/github/downloader/ParallelDownloader.scala
@@ -1,15 +1,12 @@
 package com.github.downloader
 
 import java.io._
-import java.nio.file.{Files, Paths}
-import java.util.Collections
+import java.nio.file.Paths
 
 import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.FileIO
-import akka.stream.{ActorMaterializer, IOResult}
 
-import scala.collection.JavaConverters._
-import scala.collection.immutable
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
@@ -19,30 +16,15 @@ case class ParallelDownloader() {
 
   def startDownload(url: String, offset: Long): Unit = {
     val fileName = Paths.get(url).getFileName.toString
+    val partFileNamePrefix = s"part-$fileName"
 
-    val result: immutable.Seq[Future[IOResult]] = RemoteResource(url).asParallelStream()
+    Future.sequence(RemoteResource(url).asParallelStream()
       .map(s => s.map(pr => pr.byteString))
-      .zipWithIndex.map({ case (stream, index) => FileIO.toPath(new File(s"part-$fileName-$index").toPath)
+      .zipWithIndex.map({ case (stream, index) => FileIO.toPath(new File(s"$partFileNamePrefix-$index").toPath)
       .runWith(stream)
+    })).onComplete(_ => {
+      FileMerger(partFileNamePrefix, fileName).merge()
+      system.terminate()
     })
-
-    Future.sequence(result)
-      .onComplete(_ => {
-        val files = new File(".").listFiles()
-          .toList
-          .filter(f => f.getName.startsWith("part-"))
-          .sorted
-
-        val streams = files.map(f => new FileInputStream(f)).asJavaCollection
-        val in = new SequenceInputStream(Collections.enumeration[FileInputStream](streams))
-
-        Files.copy(in, Paths.get(fileName))
-
-        streams.forEach(f => f.close())
-        files.foreach(f => f.delete())
-        in.close()
-
-        system.terminate()
-      })
   }
 }
