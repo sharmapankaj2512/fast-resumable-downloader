1fb230a241165a910c8c4c98e13fa34dd6df1018
Pankaj Sharma <sharmapankaj2512@gmail.com>
2017-12-29 11:50:06 +0530

    Move CommandlineDownloader to examples

--- examples/src/main/scala/com/github/downloader/examples/DownloadVideoFile.scala
+++ examples/src/main/scala/com/github/downloader/examples/DownloadVideoFile.scala
@@ -1,9 +1,16 @@
 package com.github.downloader.examples
 
-import com.github.downloader.CommandLineDownloader
+import com.github.downloader.{RemoteResource, SequentialDownloader}
+import com.github.subscriber.{CommandLineProgressBar, File}
 
 object DownloadVideoFile extends App {
   val url = "http://mirrors.standaloneinstaller.com/video-sample/jellyfish-25-mbps-hd-hevc.3gp"
 
-  CommandLineDownloader.download(url)
+  val file = File(url)
+  val downloadedSize = file.downloadedSize()
+  val remoteResource = RemoteResource(url)
+  val progressBar = CommandLineProgressBar(remoteResource.size())
+
+  progressBar.tick(downloadedSize)
+  SequentialDownloader(List(progressBar, file)).download(remoteResource, downloadedSize)
 }
